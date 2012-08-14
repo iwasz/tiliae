ef70c9bfb4ee21e5756017aa253afae53f276919
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2012-08-14 22:11:33 +0000

    topologicalsort seems to be OK

--- src/container/inputFormat/compact/test/CompactTest15.cc
+++ src/container/inputFormat/compact/test/CompactTest15.cc
@@ -34,24 +34,44 @@ BOOST_AUTO_TEST_CASE (test071TopologicalSort)
         Ptr <MetaContainer> m = CompactMetaService::parseFile (PATH + "071-topological-sort.xml");
 
         MetaMap &mm = const_cast <MetaMap &> (m->getMetaMap ());
-        BOOST_CHECK_EQUAL (mm.size (), 3U);
+        BOOST_CHECK_EQUAL (mm.size (), 8U);
 
-        BOOST_CHECK (mm.find ("thisCity") != mm.end ());
-        BOOST_CHECK (mm["thisCity"]);
+        BOOST_CHECK (mm.find ("a") != mm.end ());
+        BOOST_CHECK (mm["a"]);
 
-        BOOST_CHECK (m->get ("thisCity"));
-        BOOST_CHECK (m->get ("kinkreet0"));
-        BOOST_CHECK (m->get ("kinkreet2"));
+        BOOST_CHECK (m->get ("a"));
+        BOOST_CHECK (m->get ("b"));
+        BOOST_CHECK (m->get ("c"));
 }
 
 BOOST_AUTO_TEST_CASE (test072TopologicalSort)
 {
         Ptr <MetaContainer> m = CompactMetaService::parseFile (PATH + "071-topological-sort.xml");
-        std::cerr << *m << std::endl;
+//        std::cerr << *m << std::endl;
 
         MetaDeque sorted = m->topologicalSort ();
-        BOOST_CHECK_EQUAL (sorted.size (), 3U);
-        std::cerr << sorted << std::endl;
+        BOOST_CHECK_EQUAL (sorted.size (), 8U);
+//        std::cerr << sorted << std::endl;
+}
+
+struct Dummy {
+
+        REFLECTION_CONSTRUCTOR_ (void)
+
+        Dummy *REFLECTION_FIELD_VALUE_INPLACE (field1);
+        Dummy *REFLECTION_FIELD_VALUE_INPLACE (field2);
+        Dummy *REFLECTION_FIELD_VALUE_INPLACE (field3);
+
+        REFLECTION_END (Dummy)
+};
+
+BOOST_AUTO_TEST_CASE (test073TopologicalSort)
+{
+        Ptr <MetaContainer> m = CompactMetaService::parseFile (PATH + "073-topological-sort.xml");
+//        std::cerr << *m << std::endl;
+
+        MetaDeque sorted = m->topologicalSort ();
+//        std::cerr << sorted << std::endl;
 }
 
 BOOST_AUTO_TEST_SUITE_END ();

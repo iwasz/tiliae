a9cc21dc3f7e8d7343ad030476f6c35147cafc7a
liwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2015-09-29 14:03:59 +0200

    First successful clang build

--- configure.sh
+++ configure.sh
@@ -3,4 +3,4 @@
 #export CXX=/home/iwasz/local/share/clang-3.6.0/bin/clang++
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE='Debug' -DCMAKE_INSTALL_PREFIX:PATH=$HOME/local -DBUILD_SHARED_LIBS=on -DBUILD_TESTS=0 -DBUILD_DEMO=0 -DLLVM_ROOT="/home/liwaszkiewicz/local/share/llvm/" ..
+cmake -DCMAKE_BUILD_TYPE='Debug' -DCMAKE_INSTALL_PREFIX:PATH=$HOME/local -DBUILD_SHARED_LIBS=on -DBUILD_TESTS=0 -DBUILD_DEMO=0 -DLLVM_ROOT="/home/liwaszkiewicz/local/share/llvm/" -DBOOST_ROOT="/home/liwaszkiewicz/local" ..
--- tiliae.cmake
+++ tiliae.cmake
@@ -2,15 +2,7 @@
 # | Generate reflection database. |
 # +-------------------------------+
 FUNCTION (TILIAE_PARSE PARSER_PATH INPUT_FILE OUTPUT_FILE)
-        SET (DATABASE_DIR "${CMAKE_CURRENT_BINARY_DIR}/reflectionDatabase/${INPUT_FILE}")
-        SET (DATABASE_FILE "${DATABASE_DIR}/ReflectionDatabase.cc")
-        FILE (MAKE_DIRECTORY ${DATABASE_DIR})
-
-        #MESSAGE (STATUS "Using reflection parser : ${PARSER_PATH}")
-
-        ADD_CUSTOM_COMMAND (OUTPUT ${DATABASE_FILE}
-                COMMAND ${PARSER_PATH} -p compile_commands.json "${CMAKE_CURRENT_SOURCE_DIR}/${INPUT_FILE}" -o ${DATABASE_FILE}
-                DEPENDS ${INPUT_FILE} tiliaeparser)
-
-        SET (${OUTPUT_FILE} ${DATABASE_FILE} PARENT_SCOPE)
+        ADD_CUSTOM_COMMAND (OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/${OUTPUT_FILE}"
+                COMMAND ${PARSER_PATH} -p compile_commands.json "${CMAKE_CURRENT_SOURCE_DIR}/${INPUT_FILE}" -o "${CMAKE_CURRENT_SOURCE_DIR}/${OUTPUT_FILE}"
+                DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${INPUT_FILE}" tiliaeparser)
 ENDFUNCTION ()
